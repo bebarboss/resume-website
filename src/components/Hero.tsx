a3631a52f8a879882e343625a4d85eb5e28a2bb8
bebarboss <bluebus.boss@gmail.com>
2025-06-22 20:42:19 +0700

    add resume link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,8 +53,8 @@ export default function Hero() {
             </a>
           </div>
           <a
-            href="/Resume_Navarit_Rdocuments/resume.pdf"
-            download="C:\Users\blueb\Desktop\projects\resume-website\public\documents\Resume_Navarit_R.pdf"
+            href="https://www.canva.com/design/DAGqnqiRlQs/Nc1-WtzvG0yYYq7LHOpeYA/edit?utm_content=DAGqnqiRlQs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+            target="_blank"
             className="bg-green-600 text-white px-6 py-2 rounded-lg hover:bg-green-700 transition-colors flex items-center"
           >
             <svg
@@ -71,7 +71,7 @@ export default function Hero() {
                 d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
               />
             </svg>
-            Download My Resume
+            View My Resume
           </a>
         </div>
       </div>
